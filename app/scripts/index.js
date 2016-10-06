9a90b90a9bc9fc599f1bfb747a2f996f3750b960
Gregory Shuford <bearshuford@me.com>
2016-10-05 22:07:49 -0400

    poduct grid completed & javascript implimented

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -18,6 +18,71 @@ var handlebars = require('handlebars');
 
 var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=zine&includes=Images,Shop&sort_on=score"
 
+
+
+
+var source   = $("#flex-stamp").html();
+var template = handlebars.compile(source);
+var html;
+
+var context = [];
+var count = 16;
+// for(var i=0; i<count; i++){
+//
+//   var card = {
+//     'image': 'https://placecage.com/'+(340+(i*2))+'/'+(270+i)
+//   }
+//
+//   context.push(card) ;
+// }
+//
+// context.forEach(function(card){
+//   html = template(card);
+//   $('.flex-grid').append(html);
+// });
+
+
+
+
+var result = [];
+
+function logData(data) {
+   console.log(data);
+
+   for(var i=0; i<data.results.length; i++){
+      html = template(
+         {
+            'image': data.results[i].Images[0].url_170x135,
+            'title': data.results[i].title,
+            'price': data.results[i].price,
+            'url':  data.results[i].url,
+            'shop':
+               {
+                  'name': data.results[i].Shop.shop_name,
+                  'url':  data.results[i].Shop.url
+               }
+         });
+      $('.flex-grid').append(html);
+
+   }
+}
+
+
+
+// result.forEach(function(card){
+//    console.log("Card:"+card);
+//    console.log(template(card));
+//   html = template(card);
+//   $('.flex-grid').append(html);
+// });
+
+fetchJSONP(url, logData);
+
+
+
+
+
+
 /*
   (url: String, callback: Function) -> undefined
 
